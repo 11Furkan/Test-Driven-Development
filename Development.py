9dcb6a60fbab58c02f7742d8754edb868a2ad107
Furkan Yaglidere <115979551+11Furkan@users.noreply.github.com>
2023-08-21 19:01:57 +0200

    test

---
+++ Development.py
@@ -0,0 +1,12 @@
+def numEven():
+    if (num % 2) == 0:
+        print("is a even number")
+        # print((num = str) + "is a even number")
+    else:
+        print("Number isn't even")
+        # print("{num} isn't a even number")
+        
+num = 6
+# num = int(input("enter a number: "))
+
+numEven()
---
+++ test_Development.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Development import numEven
+
+class TestMain(unittest.TestCase):
+
+    def eingabe_ist_grade(self):
+        self.assertEqual(0)
+
+if __name__ == "__main__":
+    unittest.main()
